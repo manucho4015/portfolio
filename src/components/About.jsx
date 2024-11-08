afe41c9c4bdfd441683380dd1e8386a9ff078365
Emmanuel Ochieng <eochieng16@gmail.com>
2024-11-08 12:50:17 +0300

    minor styling bug fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className=''>Introduction</p>
+        <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
       <motion.p variants={fadeIn('', '', 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
