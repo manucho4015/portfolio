6e70ef062ee6fd4cffb37342c2557b47a66bb32d
Emmanuel Ochieng <eochieng16@gmail.com>
2025-08-05 15:04:34 +0300

    use valid hex color code

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -12,7 +12,7 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.5]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial color='#fffeb' polygonOffset polygonOffsetFactor={-5} flatShading />
+        <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading />
         <Decal flatShading rotation={[2 * Math.PI, 0, 6.25]} position={[0, 0, 1]} map={decal} />
       </mesh>
     </Float>
