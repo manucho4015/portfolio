3ecdf6f98603cb50bf5f59dd67b73b8bad2bf287
Emmanuel Ochieng <eochieng16@gmail.com>
2025-08-05 14:18:58 +0300

    integrate whatsapp number

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { motion } from "framer-motion";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, StarsCanvas, Works } from './components'
 
@@ -23,6 +24,11 @@ const App = () => {
           <Contact />
           <StarsCanvas />
         </div>
+        {/* Whatsapp Integration */}
+        <a href="https://wa.me/254708928256" target='_blank' rel="noopener noreferrer">
+          <motion.img drag dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }} dragTransition={{ bounceStiffness: 250, bounceDamping: 10 }} dragElastic={0.5} whileDrag={{ cursor: "grabbing" }}
+            src="/whatsapp-logo.png" alt="whatsapp" className='fixed bottom-[5vh] right-[5vw] md:bottom-[5vh] md:right-[5vw] inline-block h-305px] md:h-[50px] w-[30px] md:w-[50px] z-[20]' />
+        </a>
       </div>
     </BrowserRouter>
   )
