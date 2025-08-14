1d363ebfcad017dfd32e4cd96a196c5eac1e7295
Emmanuel Ochieng <eochieng16@gmail.com>
2025-08-14 13:06:30 +0300

    remove unnecessary fullstop

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -90,7 +90,7 @@ const Contact = () => {
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
       <motion.div variants={slideIn('left', 'tween', 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
 
         <form action="" ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
           <label htmlFor="" className='flex flex-col'>
